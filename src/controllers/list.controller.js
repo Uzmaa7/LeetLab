e0e7ab7fd510f4effad51bf296e16fc0aabacfa1
Uzmaa7 <uk9971khan@gmail.com>
2026-02-10 17:50:13 +0530

    add getAList controller using deep populate for relational data

--- src/controllers/list.controller.js
+++ src/controllers/list.controller.js
@@ -47,4 +47,36 @@ const getAllLists = async(req, res) => {
         });
     }
 }
-export {createList, getAllLists};
+
+const getAList = async (req, res) => {
+    try {
+        const {listId} = req.params;
+        const user = req.user;
+        const list = await List.findOne({_id: listId, createdBy: user._id, })
+        .populate({
+                path: 'problemsInList', 
+                populate: {
+                    path: 'problem' 
+                }
+        });
+
+        if(!list){
+            return res.status(404).json({
+                message: "List not found",
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "A list fetched successfully",
+            list
+        });
+    } catch (error) {
+        console.error("getAList", error);
+        res.status(500).json({ 
+            success: false, 
+            message: "Failed to fetch A List" 
+        });
+    }
+}
+export {createList, getAllLists, getAList};
