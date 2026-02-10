cd4ee4eb1cf60c65ced4c8cf2ef55689b94ebed2
Uzmaa7 <uk9971khan@gmail.com>
2026-02-10 16:45:05 +0530

    feat: implement full CRUD routes for playlist and problem management

--- src/routes/list.routes.js
+++ src/routes/list.routes.js
@@ -5,8 +5,15 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 const listRouter = express.Router();
 
 listRouter.get("/", verifyJWT, getAllLists);
+
 listRouter.get("/:listId", verifyJWT, getAList);
+
 listRouter.post("/create-playlist", verifyJWT, creatList);
 
+listRouter.post("/:listId/add-problem", verifyJWT, addProblemInList);
+
+listRouter.delete("/:listId", verifyJWT, deleteList);
+
+listRouter.delete("/:listId/remove-problem", verifyJWT, removeProblemFromList);
 
 export default listRouter;
