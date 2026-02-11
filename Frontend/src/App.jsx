9b9d4de1b6653107af9f420b962c58a6f06f9c8b
Uzmaa7 <uk9971khan@gmail.com>
2026-02-11 19:03:32 +0530

    feat: implement protected routes and authentication redirect logic

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
+
+import HomePage from "./pages/HomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+
+
+
 const App = () => {
+
+  const authUser = null;
+  
   return (
     <div className="flex flex-col items-center justify-start">
       <Routes>
+          <Route path="/" element = { authUser ? <HomePage/> : <Navigate to={"/login"}/> }  />
 
+          <Route path="/login" element = { !authUser ? <LoginPage/> : <Navigate to={"/"}/> } />
+
+          <Route path="/signup" element = { !authUser ? <SignUpPage/> : <Navigate to={"/"}/> } />
       </Routes>
     </div>
   )
---
+++ Frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SignUpPage() {
+
+    return(
+        <div>
+
+        </div>
+    )
+    
+}
+
+export default SignUpPage;
