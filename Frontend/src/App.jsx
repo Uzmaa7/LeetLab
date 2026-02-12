452e33876567defc25916f437d502377073732ff
Uzmaa7 <uk9971khan@gmail.com>
2026-02-12 13:47:47 +0530

    complete AuthStore with signup, login, and logout actions using Zustand

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-
+import {Toaster} from "react-hot-toast"
 
 import HomePage from "./pages/HomePage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
+import { useAuthStore } from "./store/useAuthStore.js";
+import { Loader } from "lucide-react";
 
 
 
 const App = () => {
+  const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
 
-  const authUser = null;
+  useEffect(()=>{
+    checkAuth()
+  },[checkAuth])
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+  }
   
   return (
     <div className="flex flex-col items-center justify-start">
+      <Toaster/>
       <Routes>
           <Route path="/" element = { authUser ? <HomePage/> : <Navigate to={"/login"}/> }  />
 
