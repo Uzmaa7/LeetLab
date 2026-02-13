3fdeb89b34b5ef9c750c6fc106927ccbe8be3a9f
Uzmaa7 <uk9971khan@gmail.com>
2026-02-13 16:10:53 +0530

    implement express-validator rules for auth routes
    Added registrationValidation with email, username, and password checks.
    
    Added loginValidation for email and password fields.

---
+++ Backend/src/validators/auth.Validators.js
@@ -0,0 +1,48 @@
+import { body } from "express-validator";
+
+
+
+
+const registrationValidation =  () => {
+    return [
+        body("email")
+        .trim()
+        .notEmpty().withMessage("Email is required")
+        .isEmail().withMessage("Invalid email"),
+
+        body("username")
+        .trim()
+        .notEmpty().withMessage("username is required")
+        .isLength({min:3}).withMessage("username must be atleast 3 characters long")
+        .isLength({max:20}).withMessage("username can be atmost 20 characters long"),
+
+        body("fullname")
+        .trim()
+        .notEmpty().withMessage("fullname is required"),
+
+
+        body("password")
+        .trim()
+        .notEmpty().withMessage("please set your password")
+        .isLength({min:8}).withMessage("password must be atleast 8 characters long"),
+    ]
+}
+
+
+const loginValidation = () => {
+    return [
+        body("email")
+        .trim()
+        .notEmpty().withMessage("email is required")
+        .isEmail().withMessage("Invalid email"),
+
+        body("password")
+        .trim()
+        .notEmpty().withMessage("password is required")
+    ]
+}
+
+
+
+
+export {registrationValidation, loginValidation};
