76c31a099da58afd2095684e33d32214b608659b
Uzmaa7 <uk9971khan@gmail.com>
2026-02-18 13:49:55 +0530

    - Implemented express-validator for group creation, member management, and attachments. - Added param-based validation for chat IDs and body validation for group metadata. - Updated routes to include validation middleware for better error handling.

--- Backend/src/routes/chat.routes.js
+++ Backend/src/routes/chat.routes.js
@@ -6,9 +6,13 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { createGroup, getMyChats, getMyGroups, addMembers, removeMember, exitGroup, 
 sendAttachment, getChatDetails, renameChat, deleteChat, getMessage } from "../controllers/chat.controller.js";
 
-import { createGroupChatValidation, addMembersValidation } from "../validators/chat.Validators.js";
+import { createGroupChatValidation, addMembersValidation, removeMemberValidation, chatIdValidation, 
+    sendAttachmentValidation, renameChatValidation
+ } from "../validators/chat.Validators.js";
+
 import { validate } from "../middlewares/validator.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
+
 
 const chatRouter = express.Router();
 
@@ -21,17 +25,19 @@ chatRouter.get("/my-groups", verifyJWT, getMyGroups);
 
 chatRouter.put("/add-members", verifyJWT, addMembersValidation(), validate, addMembers);
 
-chatRouter.delete("/remove-member", verifyJWT, removeMember);
+chatRouter.delete("/remove-member", verifyJWT, removeMemberValidation(), validate, removeMember);
 
-chatRouter.delete("/exit-group/:id", verifyJWT, exitGroup);
+chatRouter.delete("/exit-group/:id", verifyJWT, chatIdValidation(), validate,  exitGroup);
 
-chatRouter.post("/send-attachment", verifyJWT, upload.array("files", 5),  sendAttachment);
+chatRouter.post("/send-attachment", verifyJWT, sendAttachmentValidation, validate, upload.array("files", 5),  sendAttachment);
 
-chatRouter.get("/message/:id", verifyJWT, getMessage);
+chatRouter.get("/message/:id", verifyJWT, chatIdValidation(), validate, getMessage);
+
+
 //get chat -> details, rename, delete
 chatRouter.route("/:id")
-.get(getChatDetails)
-.put(renameChat)
-.delete(deleteChat);
+.get(chatIdValidation(), validate, getChatDetails)
+.put(renameChatValidation(), validate, renameChat)
+.delete(chatIdValidation(), validate, deleteChat);
 
 export default chatRouter;
--- Backend/src/validators/chat.Validators.js
+++ Backend/src/validators/chat.Validators.js
@@ -1,4 +1,4 @@
-import {body} from "express-validator";
+import {body, param, check, query} from "express-validator";
 
 const createGroupChatValidation = () => {
     return [
@@ -13,4 +13,64 @@ const createGroupChatValidation = () => {
     ]
 }
 
-export {createGroupChatValidation};
+
+const addMembersValidation = () => {
+    return [
+        body("chatId")
+        .notEmpty().withMessage("please provide tha Chat ID"),
+
+        body("members")
+        .notEmpty().withMessage("please enter members")
+        .isArray({min : 2}).withMessage("members must be an array")
+    ]
+}
+
+const removeMemberValidation = () => {
+    return [
+        body("chatId")
+        .notEmpty().withMessage("please provide tha Chat ID")
+        .isMongoId().withMessage("Invalid chatId format"),
+
+        body("userRemoveId")
+        .notEmpty().withMessage("Please provide a userId to remove a user")
+        .isMongoId().withMessage("Invalid userId format")
+
+    ]
+}
+
+const sendAttachmentValidation = () => {
+    return [
+         body("chatId")
+        .notEmpty().withMessage("please provide tha Chat ID")
+        .isMongoId().withMessage("Invalid chatId format"),
+
+        check("files")
+        .notEmpty().withMessage("Please provide attachment")
+        .isArray({min:1, max: 5}).withMessage("attachment must be an array of 1-5 ")
+    ]
+}
+
+const chatIdValidation = () => {
+    return [
+        param("id")
+        .notEmpty().withMessage("please provide tha Chat ID")
+        .isMongoId().withMessage("Invalid chatId format"),
+    ]
+}
+
+const renameChatValidation = () => {
+    return [
+        param("id")
+        .notEmpty().withMessage("please provide tha Chat ID")
+        .isMongoId().withMessage("Invalid chatId format"),
+
+        body("name")
+        .notEmpty().withMessage("Please enter a name")
+        .isString().withMessage("Invalid name format")
+        .isLength({min:1, max:20}).withMessage("Name must be between 1 and 20 characters")
+
+    ]
+}
+
+export {createGroupChatValidation, addMembersValidation, removeMemberValidation,
+      sendAttachmentValidation,chatIdValidation, renameChatValidation };
