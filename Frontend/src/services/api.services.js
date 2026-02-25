d64fa88451301d9e91a25cf4a05b1a83f3ffccb5
Uzmaa7 <uk9971khan@gmail.com>
2026-02-25 23:14:51 +0530

    feat: setup axios instance with environment-based baseURL
    - Created central axios instance with conditional baseURL (Render/Localhost).
    - Implemented 'setAuthToken' utility to manage Authorization headers dynamically.
    - Enabled 'withCredentials' for secure cookie-based cross-origin requests.
    - Integrated environment variables for production and local API endpoints.

---
+++ Frontend/src/services/api.services.js
@@ -0,0 +1,19 @@
+
+import axios from "axios";
+// 1. Determine the URL based on the environment
+const baseURL = import.meta.env.PROD 
+  ? (import.meta.env.VITE_PROD_API || "https://portfolio.onrender.com")  // Uses Render link when deployed
+  : (import.meta.env.VITE_LOCAL_API || "http://localhost:8080"); // Uses Localhost when running locally
+
+export const api = axios.create({
+  baseURL: `${baseURL}/api/v1`, 
+  withCredentials: true,
+});
+
+export const setAuthToken = (token) => {
+  if (token) {
+    api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } else {
+    delete api.defaults.headers.common["Authorization"];
+  }
+};
