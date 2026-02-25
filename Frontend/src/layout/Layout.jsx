3616f2f58f4d4efcd37d63ba1530fdc1380dd062
Uzmaa7 <uk9971khan@gmail.com>
2026-02-25 23:24:55 +0530

    feat: add Layout component with conditional Footer rendering

--- Frontend/src/layout/Layout.jsx
+++ Frontend/src/layout/Layout.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Navbar from '../components/Navbar'
+import { Outlet , useLocation} from "react-router-dom";
+// import Navbar from "../components/Navbar"; // Navbar hum abhi banayenge
+import { Header, Footer } from "../components/index"
 
 const Layout = () => {
-  return (
-    <div>
-        <Navbar/>
-        <Outlet/>
-    </div>
-  )
-}
+  const location = useLocation();
 
-export default Layout
+  // Jin paths par footer nahi chahiye, unhe is array mein add kar dein
+  const pathsWithoutFooter = ["/user/dashboard"];
+
+  // Check karein ki current path array mein hai ya nahi
+  const shouldHideFooter = pathsWithoutFooter.includes(location.pathname)
+
+
+  return (
+    <>
+        <Header/>
+        
+        <main className="pt-20"> 
+    <Outlet/>
+</main>
+
+        
+        {/*if  shouldHideFooter is false then render Footer  */}
+      {!shouldHideFooter && (
+        <div className="hidden lg:block">
+          <Footer />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Layout;
