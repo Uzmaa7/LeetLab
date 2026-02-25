27fa579e8c65be5682b4f69a227241595b06fc75
Uzmaa7 <uk9971khan@gmail.com>
2026-02-25 23:18:19 +0530

    feat: add Footer component with responsive layout and social links
    - Implemented a modern dark-themed footer with glassmorphism borders.
    - Added organized link sections for Product, Company, and Legal.
    - Integrated social media icons (Github, Twitter, Linkedin, Instagram) with hover animations.
    - Included dynamic branding and credit section with custom styling.
    - Ensured full responsiveness using Tailwind CSS grid and flexbox.

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,13 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter} from "react-router-dom"
+import {  RouterProvider} from "react-router-dom"
 import './index.css'
-import App from './App.jsx'
+import {router} from "./routes/index.routes" 
+import { UserContextProvider } from './contexts/UserContext.jsx'
+
+
 
 createRoot(document.getElementById('root')).render(
+
   <StrictMode>
-    <BrowserRouter>
-       <App />
-    </BrowserRouter> 
-  </StrictMode>,
+    
+    <UserContextProvider>
+        <RouterProvider router={router}/>
+    </UserContextProvider>
+    
+  </StrictMode>
 )
