a65c5cb9ab09b04630a1e1ae4ec7826ef22c1077
Uzmaa7 <uk9971khan@gmail.com>
2026-03-07 00:35:03 +0530

    add note on route ordering to prevent dynamic parameter conflicts

--- Backend/src/routes/collectionQuestion.routes.js
+++ Backend/src/routes/collectionQuestion.routes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { validate } from "../middlewares/validator.middleware.js";
 import { addQuestionToCollectionValidator, idValidator, bulkAddQuestionsValidator } from "../validators/collection.Validators.js";
-import { addQuestionToCollection, bulkAddQuestions, removeQuestionFromCollection } from "../controllers/collectionQuestion.controller.js";
+import { addQuestionToCollection, bulkAddQuestions, removeQuestionFromCollection, bulkRemoveQuestions } from "../controllers/collectionQuestion.controller.js";
 
 const collectionQuestionRouter = express.Router();
 
@@ -11,12 +11,14 @@ const collectionQuestionRouter = express.Router();
 //add a question  the collection
 collectionQuestionRouter.post("/:collectionId/questions",verifyJWT, addQuestionToCollectionValidator(), validate, addQuestionToCollection);
 
+//bulk add question
+collectionQuestionRouter.post("/:collectionId/questions/bulk", verifyJWT, idValidator(), bulkAddQuestionsValidator(), validate, bulkAddQuestions);
+
+//bulk remove question
+collectionQuestionRouter.delete("/:collectionId/questions/bulk", verifyJWT, idValidator(), bulkAddQuestionsValidator(), validate, bulkRemoveQuestions)
+
 //remove a question from collection
 collectionQuestionRouter.delete("/:collectionId/questions/:questionId",verifyJWT, idValidator(), validate, removeQuestionFromCollection);
-
-//bull add question
-collectionQuestionRouter.post("/:collectionId/questions/bulk", verifyJWT, idValidator(), bulkAddQuestionsValidator(), validate, bulkAddQuestions);
-//bulk remove question
 
 //remove all questions from the collection
 
