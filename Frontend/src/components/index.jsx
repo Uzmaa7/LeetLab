3c8c0e28328472ef73a7ab5b97ecd159268cdc29
Uzmaa7 <uk9971khan@gmail.com>
2026-03-08 16:20:24 +0530

    Feat(ui): implement DashboardLayout with dynamic tab navigation and slots

--- Frontend/src/components/index.jsx
+++ Frontend/src/components/index.jsx
@@ -2,8 +2,14 @@ import Header from "./Header/Header.jsx";
 
 import Footer from "./Footer/Footer.jsx";
 import Input from "./Other/Input.jsx";
-import LeetButton from "../components/Other/Button.jsx";
+import LeetButton from "../components/Other/LeetButton.jsx";
 
 import UserMenu from "./Header/UserMenu.jsx";
+import TreeAnimation from "./TreeAnimation.jsx";
 
-export {Header, Footer, Input, UserMenu, LeetButton};
+export {Header, Footer, Input, UserMenu, LeetButton, TreeAnimation};
+
+
+
+
+
